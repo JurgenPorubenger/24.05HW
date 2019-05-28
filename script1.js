9dc71e4f988856a017a2ecde26cce2bd8e36598f
Eugene <jurgen.porubenger@gmail.com>
2019-05-28 17:25:08 +0300

    latest

--- script1.js
+++ script1.js
@@ -6,24 +6,34 @@ function plus() {
    let b1 = document.getElementById("second_number").value;
    b1 = parseInt(b1);
 
-   let result = a1+b1;
+   let plusButton = document.getElementById("plus");
+   let minusButton = document.getElementById("minus");
 
-   document.getElementById("result").innerHTML=result;
+   if (plusButton.onclick) {
+      let result = a1+b1; 
+      document.getElementById("result").innerHTML=result;
+   } 
+   else if (minusButton.onclick) {
+      let result = a1-b1;
+      document.getElementById("result").innerHTML=result;
+   }
+
+   
    
 }
-function minus() {
+// function minus() {
 
-   let a1 = document.getElementById("first_number").value;
-   a1 = parseInt(a1);
+//    let a1 = document.getElementById("first_number").value;
+//    a1 = parseInt(a1);
 
-   let b1 = document.getElementById("second_number").value;
-   b1 = parseInt(b1);
+//    let b1 = document.getElementById("second_number").value;
+//    b1 = parseInt(b1);
 
-   let result1 = a1-b1;
+//    let result1 = a1-b1;
 
-   document.getElementById("result").innerHTML=result1;
-}
-
+//    document.getElementById("result").innerHTML=result1;
+// }
+ 
 
 // function plus(a,b) {
 //     a = document.getElementById("first_number").value;
